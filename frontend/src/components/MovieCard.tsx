e73eb2dafb918f117416f58e9cd00ccad40ae95a
Peter.Anastasov7 <peter.dim.an7@gmail.com>
2025-08-04 14:45:52 +0300

    Add navigation to movie details

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -1,19 +1,27 @@
+import { Route, useNavigate } from 'react-router-dom'
 import styles from './MovieCard.module.css'
 
 type Movie = {
     title: string
     category: string
     image: string
+    id: string
 }
 
-const MovieCard = ({ title, category, image }: Movie) => {
+const MovieCard = ({ title, category, image, id }: Movie) => {
+    const navigate = useNavigate()
+
+    const handleInfoClick = () => {
+        navigate(`/${id}`)
+    }
+
     return (
         <div className={styles.card}>
             <img src={image} alt={title} className={styles.image} />
             <h2 className={styles.title}>{title}</h2>
             <p className={styles.category}>{category}</p>
             <div style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
-                <button className={styles['info-btn']}>Info</button>
+                <button onClick={handleInfoClick} className={styles['info-btn']}>Info</button>
                 <button className={styles['delete-btn']}>Delete</button>
             </div>
         </div>
